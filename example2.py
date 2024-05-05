6c6882e4988c0ea531f70b79d0965fb13ce368a0
GigiMad <ang.gian0711@gmail.com>
2024-05-05 20:11:03 +0200

    pdf saved
    data saved

---
+++ example2.py
@@ -0,0 +1,21 @@
+
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+import json
+import os
+
+json_path = os.path.join("penautomate_menu", "pentestreport", "pentestreport2.json")
+
+def generate_pdf(filename, data):
+    c = canvas.Canvas(filename, pagesize=letter)
+    y_position = 750
+    for key, value in data.items():
+        text = f"{key}: {value}"
+        c.drawString(100, y_position, text)
+        y_position -= 30  # Move to the next line
+    c.save()
+
+if __name__ == "__main__":
+    with open(json_path, 'r') as file:
+        json_data = json.load(file)
+    generate_pdf("result.pdf", json_data)
