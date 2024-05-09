673d3ee029a93d552ba6dff597d77fe8ca2a4c00
GigiMad <ang.gian0711@gmail.com>
2024-05-09 04:28:36 +0200

    report v1

--- example2.py
+++ example2.py
@@ -1,21 +1,59 @@
-
+import json
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
-import json
+from reportlab.lib import colors
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, Spacer
+from reportlab.lib.styles import getSampleStyleSheet
 import os
 
-json_path = os.path.join("penautomate_menu", "pentestreport", "pentestreport.json")
+json_input_path = os.path.join("penautomate_menu", "pentestreport", "pentestreport.json")
 
-def generate_pdf(filename, data):
-    c = canvas.Canvas(filename, pagesize=letter)
-    y_position = 750
-    for key, value in data.items():
-        text = f"{key}: {value}"
-        c.drawString(100, y_position, text)
-        y_position -= 30  # Move to the next line
-    c.save()
 
-if __name__ == "__main__":
-    with open(json_path, 'r') as file:
-        json_data = json.load(file)
-    generate_pdf("pentestreport.pdf", json_data)
+# Charger les données JSON
+with open(json_input_path, 'r') as file:
+    data = json.load(file)
+
+# Créer un fichier PDF
+pdf_report = SimpleDocTemplate("pentest_report.pdf", pagesize=letter)
+flowables = []
+styles = getSampleStyleSheet()
+
+# Titre du document
+title = Paragraph('Pentest Report', styles['Title'])
+flowables.append(title)
+flowables.append(Spacer(1, 12))
+
+# Ajouter des données de base
+base_data = [
+    ["IP Address", data["ip_address"]],
+    ["Hostname", data["hostname"]],
+    ["Location", f'{data["city"]}, {data["region"]}, {data["country"]}'],
+    ["Organization", data["organization"]],
+    ["Timezone", data["timezone"]]
+]
+table = Table(base_data, style=TableStyle([
+    ('BACKGROUND', (0, 0), (-1, 0), colors.gray),
+    ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+    ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+    ('FONTNAME', (0, 0), (-1, -1), 'Helvetica-Bold'),
+    ('BOTTOMPADDING', (0, 0), (-1, -1), 12),
+]))
+flowables.append(table)
+flowables.append(Spacer(1, 12))
+
+# Information sur les sous-domaines
+subdomain_data = [["Subdomain", "IP Address"]]
+subdomain_data.extend([[sd["subdomain"], sd["ip"]] for sd in data["subdomain_details"]])
+table = Table(subdomain_data, style=TableStyle([
+    ('GRID', (0, 0), (-1, -1), 1, colors.black),
+    ('BACKGROUND', (0, 0), (-1, 0), colors.lightblue),
+    ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+    ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+    ('FONTNAME', (0, 0), (-1, -1), 'Helvetica-Bold'),
+    ('BOTTOMPADDING', (0, 0), (-1, -1), 12),
+]))
+flowables.append(table)
+
+# Enregistrer le PDF
+pdf_report.build(flowables)
+print("Le rapport PDF a été créé avec succès.")
